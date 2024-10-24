82945e993bffbda20c5a6d5c6a87fccaee4a846b
pasahgroup <info@pasah.net>
2024-10-24 15:08:59 +0300

    24 10 2024

--- resources/views/website/layouts/apps.blade.php
+++ resources/views/website/layouts/apps.blade.php
@@ -204,72 +204,12 @@
                                     </ul> -->
                                 </li>
 
+    <li><a href="#" class="btn btn-primary" style="color:#pink">My Safari</a>
 
 
-                                     <li><a href="#"> <i class="fa fa-search" style="color:pink;"></i></a>
-                                    <div class="megamenu">
-                                        <ul class="single-mega cn-col-12">
-                                            <li class="title">Search by selecting program</li>
-                                           
-                                       
-  <div class="south-search-area">
-            <div class="row wppadding">
-                <div class="col-12">
-                    <div class="advanced-search-form">
-                      
-                        <!-- Search Form -->
-                        <!-- <form action="#" method="post" id="advanceSearch"> -->
-                             <form  method="post"  action="{{ route('search-tour') }}" enctype="multipart/form-data">
-                                  @csrf
-                            <div class="row wpadding">
-                                <input type="hidden" name="_method" value="POST">
-                                 <div class="col-12 col-md-6 col-lg-6">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" name="search" placeholder="any keyword">
-                                    </div>
-                                </div>
-                              
-                                <div class="col-12 search-form-second-steps">
-                                       <div class="row">                                          <div class="col-12 col-md-6 col-lg-6">
-                                    <div class="form-group">
-                                        <input type="number" class="form-control" name="price" placeholder="maximum price">
-                                    </div>
-                                </div>
-                                     
-                                    </div>
-                                </div>
-
-                                <div class="col-12 d-flex justify-content-between align-items-end">
-                                    <!-- More Filter -->
-                                    <div class="more-filter">
-                                        <a href="#" id="moreFilter">+ More filters</a>
-                                    </div>
-                                 
-                                    <div class="form-group mb-0" target="_blank">
-                                        <button type="submit" class="btn btn-success" target="_blank" name="search2" value="search2">Search</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-        </div>
-    </div>
-     </div>
-                                
-
-</li>
-<li>||</li>
-    <li><a href="#" class="btn btn-success" style="color:#000">Create My Safari</a>
                                     <ul class="dropdown">
                                          <li><a href="/tailorForm" class="btn-outline btn-success">Create New Safari(Tailor Maide)</a></li>
                 <li><a href="/tailorClientForm" class="btn-outline btn-success">My Existing Safari-Tailor Made</a></li>
-                                                      
-                                    </ul>
-                                </li>
-
-                                 <li><a href="#" class="btn btn-outline btn-primary" style="color:#000">My Last Booking</a>
-                                    <ul class="dropdown">
                 <li><a href="/bookingTrip" class="btn-outline btn-primary">My Existing Safari</a></li>
                                                       
                                     </ul>
